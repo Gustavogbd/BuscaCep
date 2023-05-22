5937f686e0688742f9f845c892cf4ab58728e1d7
Gustavogbd <gustavokkg2@hotmail.com>
2023-05-22 14:00:51 -0300

    Método Pesquisa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,52 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app-busca-cep';
+
+  constructor( private http: HttpClient) { }
+
+  //-- variáveis do form
+  cep: string = '';
+
+  //-- Variáveis do retorno
+  endereco: any;
+  retorno = {
+    rua: ' ',
+    cidade: ' ',
+    estado: ' '
+  }
+
+  //logradouro=rua, localidade=cidade, uf=estado
+
+  pesquisado = false;
+  espera = false;
+
+  pesquisa() {
+    this.espera = true;
+    this.cep = this.cep.replace(/\D/g, '');
+
+    const url = 'http://viacep.com.br/ws/'+this.cep+'/json/';
+
+    this.http.get(url).subscribe((res) =>{
+      this.endereco = res;
+      this.retorno.rua = this.endereco.logradouro;
+      this.retorno.cidade = this.endereco.localidade;
+      this.retorno.estado = this.endereco.uf;
+      //console.log(this.endereco);
+    });
+
+    this.pesquisado = true;
+    this.espera = false;
+  }
+
+  limpa() {
+    this.pesquisado = false;
+    this.espera = false;
+    this.cep = '';
+    this.endereco = '';
+    this.retorno.rua = '';
+    this.retorno.cidade = '';
+    this.retorno.estado = '';
+  }
+
+
 }
